d8e9346b9b5c5442d7d0d77082178d0022206f88
Paulo-Teodoro <paulolimrs@outlook.com>
2020-09-01 20:31:36 -0300

    Aula dia 01/09/2020 criação model, controller e view

---
+++ WebVendas/Controllers/DepartmentsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebVendas.Models;
+
+namespace WebVendas.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Name = "Eletronics" });
+            list.Add(new Department { Id = 2, Name = "Fashion" });
+            list.Add(new Department { Id = 3, Name = "Cars" });
+            list.Add(new Department { Id = 4, Name = "Motorcicle" });
+
+            return View(list);
+        }
+    }
+}
--- WebVendas/Controllers/HomeController.cs
+++ WebVendas/Controllers/HomeController.cs
@@ -11,13 +11,7 @@ namespace WebVendas.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+       
         public IActionResult Index()
         {
             return View();
